120334ae17435f3caa6252e6f7706000e15e9d18
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-07-31 08:17:48 +0200

    Created Navbar Set up router and navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { logo } from "./assets";
+import CreatePost from "./pages/CreatePost";
+import Home from "./pages/Home";
+
 const App = () => {
-  return <h1 className='text-7xl font-bold underline'>Hello</h1>;
+  return (
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to='/'>
+          <img src={logo} alt='logo' className='w-28 object-contain' />
+        </Link>
+        <Link
+          to='/CreatePost'
+          className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'
+        >
+          Create
+        </Link>
+      </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/createPost' element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;

cbe9a3e2a02513d8db5ae8de7b1534a1e9170eb9
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-07-31 09:19:27 +0200

    page create post started

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import FormField from "../components/FormField";
+import Loader from "../components/Loader";
+
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return <div>CreatePost </div>;
 };
 
 export default CreatePost;

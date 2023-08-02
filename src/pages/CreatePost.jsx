6c123ad6065b49b621fcbbe3e6c4a1d9fd8ba571
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-02 10:46:37 +0200

    the post are send to the cloud

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -41,8 +41,26 @@ const CreatePost = () => {
     }
   };
 
-  const handleSubmit = () => {
-    console.log("submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(form),
+        });
+        await response.json();
+        navigate("/");
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please enter a request and generate an image");
+    }
   };
   const handleChange = (e) => {
     console.log("change");
@@ -54,71 +72,71 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt });
   };
   return (
-    <section className='max-w-7xl mx-auto'>
+    <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
-        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
           Create imaginative and visually stunning images trough by DALL-E AI
           and share them with the community
         </p>
       </div>
-      <form className='mt-16 max-w-3xl ' onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-5'>
+      <form className="mt-16 max-w-3xl " onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
           <FormField
-            LabelName='Your Name'
-            type='text'
-            name='name'
-            placeholder='Batman'
+            LabelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Batman"
             value={form.name}
             handleChange={handleChange}
           />
           <FormField
-            LabelName='Prompt'
-            type='text'
-            name='prompt'
-            placeholder='A synthwave style sunset above the reflecting water of the sea, digital art'
+            LabelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A synthwave style sunset above the reflecting water of the sea, digital art"
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
-          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center '>
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center ">
             {form.photo ? (
               <img
                 src={form.photo}
                 alt={form.prompt}
-                className='w-full h-full object-contain'
+                className="w-full h-full object-contain"
               />
             ) : (
               <img
                 src={preview}
-                alt='preview'
-                className='w-9/12 h-9/12 object-contain opacity-40'
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
             {generatingImg && (
-              <div className='absolute inset-0 z-0 flex justify-center items-center rounded-lg bg-[rgba(0,0,0,0.5)]'>
+              <div className="absolute inset-0 z-0 flex justify-center items-center rounded-lg bg-[rgba(0,0,0,0.5)]">
                 <Loader />
               </div>
             )}
           </div>
         </div>
-        <div className='mt-5 flex gap-5'>
+        <div className="mt-5 flex gap-5">
           <button
-            type='button'
+            type="button"
             onClick={generateImage}
-            className='text-white bg-green-700 rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center font-medium'
+            className="text-white bg-green-700 rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center font-medium"
           >
             {generatingImg ? "Genereting..." : "Generate"}
           </button>
         </div>
-        <div className='mt-10'>
-          <p className='mt-2 text-[#666e75] text-[14px]'>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
             Do you want to share your newly generated image with the community?
           </p>
           <button
-            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
-            type='submit'
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            type="submit"
           >
             {" "}
             {loading ? "Sharing..." : "Share with the community"}

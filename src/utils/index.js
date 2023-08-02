4d077fe7d31de900e956d6fc747c2734911abdb0
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-08-02 11:39:32 +0200

    The app is fully functional

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,13 @@
 import { surpriseMePrompts } from "../contstants";
+import FileSaver from "file-saver";
 
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
   return randomPrompt;
+}
+
+export default function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
